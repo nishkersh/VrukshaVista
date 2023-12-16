c7ba27455e5ddd36635a44d7759014ff190d42d9
Dhruv <dbhatt2983@gamil.com>
2023-12-16 22:59:32 +0530

    signup created

--- application/urls.py
+++ application/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload_image/', views.upload_image, name='upload_image'),
     path('google_earth/', views.google_earth, name='google_earth'),
-    # login
     path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 import requests
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
 
 def upload_image(request):
-    # if request.method == 'POST' and request.FILES['image']:
-    #     image = request.FILES['image']
-    #     fs = FileSystemStorage()
-    #     filename = fs.save(image.name, image)
-    #     uploaded_file_url = fs.url(filename)
     if request.method == 'POST' and request.FILES['image']:
         image = request.FILES['image']
         fs = FileSystemStorage()
@@ -36,4 +33,23 @@ def signin(request):
         else:
             messages.info(request, 'Incorrect login ID or password. Please try again.')
         return redirect('/dashboard')
+    return redirect("/")
+
+def signup(request):
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect("/")
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+    return redirect("/")
+
+
+@login_required
+def logout_user(request):
+    logout(request)
     return redirect("/")
