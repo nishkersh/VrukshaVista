69e49df7a6841ea6e86fecb2bc384a68c5406d9c
Dhruv <dbhatt2983@gamil.com>
2023-12-16 15:34:33 +0530

    image upload sorted

--- application/views.py
+++ application/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
     return render(request, 'index.html')
+
+def upload_image(request):
+    # if request.method == 'POST' and request.FILES['image']:
+    #     image = request.FILES['image']
+    #     fs = FileSystemStorage()
+    #     filename = fs.save(image.name, image)
+    #     uploaded_file_url = fs.url(filename)
+    if request.method == 'POST' and request.FILES['image']:
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        uploaded_file_url = fs.url(filename)
+        
+        return HttpResponse(request, uploaded_file_url)
+
+    return render(request, 'image_input.html')
