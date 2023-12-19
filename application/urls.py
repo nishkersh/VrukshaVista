5bbee805be86248886203440416ee32602c2d7a0
Dhruv <dbhatt2983@gamil.com>
2023-12-19 18:33:23 +0530

    resolving conflicts

--- application/urls.py
+++ application/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_user, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-<<<<<<< HEAD
     path('about_us/', views.about_us, name='about_us'),
-=======
     path('map/', views.map_data, name='map'),
     path('google_earth_old/', views.google_earth_old, name='google_earth_old'),
->>>>>>> 4411a582a64715377c5c4cb4da18fdb946208d63
 ]
